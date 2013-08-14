b1baf5501d3223d3bbd472e7481b6ab864c8f4e0
Jakub Zelenka <bukka@php.net>
2013-08-14 20:48:09 +0100

    Implement handlers for custom objects

--- extest_serialize.c
+++ extest_serialize.c
@@ -18,11 +18,83 @@
 
 #include "php.h"
 #include "php_extest.h"
+#include "php_extest_serialize.h"
+
+typedef struct {
+	zend_object zo;
+	int exam;
+} extest_serialize_object;
 
 PHP_EXTEST_API zend_class_entry *extest_serialize_existing_ce;
 PHP_EXTEST_API zend_class_entry *extest_serialize_virtual_ce;
 PHP_EXTEST_API zend_class_entry *extest_serialize_custom_ce;
 
+/* object handlers */
+static zend_object_handlers extest_serialize_existing_object_handlers;
+static zend_object_handlers extest_serialize_virtual_object_handlers;
+static zend_object_handlers extest_serialize_custom_object_handlers;
+
+/* {{{ extest_serialize_object_free */
+static void extest_serialize_object_free(zend_object *object TSRMLS_DC)
+{
+	extest_serialize_object *intern = (extest_serialize_object *) object;
+	zend_object_std_dtor(&intern->zo TSRMLS_CC);
+	efree(intern);
+}
+/* }}} */
+
+/* {{{ extest_serialize_object_create_ex */
+static zend_object_value extest_serialize_object_create_ex(zend_class_entry *class_type, extest_serialize_object **ptr TSRMLS_DC)
+{
+	zend_object_value retval;
+	extest_serialize_object *intern;
+
+	/* Allocate memory for it */
+	intern = (extest_serialize_object *) emalloc(sizeof(extest_serialize_object));
+	memset(intern, 0, sizeof(extest_serialize_object));
+	if (ptr) {
+		*ptr = intern;
+	}
+	zend_object_std_init(&intern->zo, class_type TSRMLS_CC);
+	object_properties_init(&intern->zo, class_type);
+
+	if (class_type == extest_serialize_existing_ce) {
+		retval.handlers = &extest_serialize_existing_object_handlers;
+	} else if (class_type == extest_serialize_virtual_ce) {
+		retval.handlers = &extest_serialize_virtual_object_handlers;
+	} else {
+		retval.handlers = &extest_serialize_custom_object_handlers;
+	}
+
+	retval.handle = zend_objects_store_put(
+		intern,
+		(zend_objects_store_dtor_t) zend_objects_destroy_object,
+		(zend_objects_free_object_storage_t) extest_serialize_object_free,
+		NULL TSRMLS_CC);
+
+	return retval;
+}
+/* }}} */
+
+/* {{{ extest_serialize_object_create */
+static zend_object_value extest_serialize_object_create(zend_class_entry *class_type TSRMLS_DC)
+{
+	return extest_serialize_object_create_ex(class_type, NULL TSRMLS_CC);
+}
+/* }}} */
+
+/* {{{ extest_serialize_object_clone */
+zend_object_value extest_serialize_object_clone(zval *this_ptr TSRMLS_DC)
+{
+	extest_serialize_object *new_obj = NULL;
+	extest_serialize_object *old_obj = (extest_serialize_object *) zend_object_store_get_object(this_ptr TSRMLS_CC);
+	zend_object_value new_ov = extest_serialize_object_create_ex(old_obj->zo.ce, &new_obj TSRMLS_CC);
+
+	zend_objects_clone_members(&new_obj->zo, new_ov, &old_obj->zo, Z_OBJ_HANDLE_P(this_ptr) TSRMLS_CC);
+
+	return new_ov;
+}
+/* }}} */
 
 /* {{{ PHP_MINIT_FUNCTION */
 PHP_MINIT_FUNCTION(extest_serialize)
@@ -31,15 +103,24 @@ PHP_MINIT_FUNCTION(extest_serialize)
 
 	/* Default serialization of object properties */
 	INIT_CLASS_ENTRY(ce, "ExtestSerializeReal", NULL);
+	ce.create_object = extest_serialize_object_create;
+	memcpy(&extest_serialize_existing_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+	extest_serialize_existing_object_handlers.clone_obj = extest_serialize_object_clone;
 	extest_serialize_existing_ce = zend_register_internal_class(&ce TSRMLS_CC);
 	zend_declare_property_null(extest_serialize_existing_ce, "algorithm", sizeof("algorithm")-1, ZEND_ACC_PUBLIC TSRMLS_CC);
 
 	/* Serailization of object properties */
 	INIT_CLASS_ENTRY(ce, "ExtestSerializeVirtual", NULL);
+	ce.create_object = extest_serialize_object_create;
+	memcpy(&extest_serialize_virtual_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+	extest_serialize_virtual_object_handlers.clone_obj = extest_serialize_object_clone;
 	extest_serialize_existing_ce = zend_register_internal_class(&ce TSRMLS_CC);
 
 	/* Custom serialization using new API */
 	INIT_CLASS_ENTRY(ce, "ExtestSerializeCustom", NULL);
+	ce.create_object = extest_serialize_object_create;
+	memcpy(&extest_serialize_custom_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+	extest_serialize_custom_object_handlers.clone_obj = extest_serialize_object_clone;
 	extest_serialize_existing_ce = zend_register_internal_class(&ce TSRMLS_CC);
 
 	return SUCCESS;
