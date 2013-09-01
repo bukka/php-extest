bfbb8b1d8d88ed07b08028207fec1db948470b74
Jakub Zelenka <jakub.zelenka@gmail.com>
2013-09-01 17:45:06 +0100

    Measure time in serialize benchmark

--- bench/serialize.php
+++ bench/serialize.php
@@ -1,20 +1,37 @@
 <?php
 
+define('EXTEST_SERIALIZE_ITERATIONS', 10000);
+
 $static = new ExtestSerializeS;
 $dynamic = new ExtestSerializeD;
 $custom = new ExtestSerializeC;
 
+function test_serialization($object) {
+	// output serialization
+	echo serialize($object) . PHP_EOL;
+	$start = microtime(true);
+	for ($j = 0; $j < EXTEST_SERIALIZE_ITERATIONS; $j++) {
+		serialize($object);
+	}
+	$end = microtime(true);
+	$duration = $end - $start;
+	printf("time for %d iterations: %f" . PHP_EOL, EXTEST_SERIALIZE_ITERATIONS, $duration);
+}
+
 for ($i = 0; $i < EXTEST_NUM_EXAMS; $i++) {
+	echo "---- EXAM $i ----" . PHP_EOL;
+
 	echo "STATIC" . PHP_EOL;
 	$static->setExam($i);
-	echo serialize($static) . PHP_EOL;
-
+	test_serialization($static);
+	
 	echo "DYNAMIC" . PHP_EOL;
 	$dynamic->setExam($i);
-	echo serialize($dynamic) . PHP_EOL;
+	test_serialization($dynamic);
 
 	echo "CUSTOM" . PHP_EOL;
 	$custom->setExam($i);
-	echo serialize($custom) . PHP_EOL;
+	test_serialization($custom);
 
+	echo PHP_EOL;
 }
