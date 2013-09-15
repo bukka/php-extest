91bf247f367cea2c95e09eda4ee28ad7ca577326
Jakub Zelenka <jakub.zelenka@gmail.com>
2013-09-15 19:32:21 +0100

    Modify serialize bench test

--- bench/serialize.php
+++ bench/serialize.php
@@ -1,21 +1,23 @@
 <?php
 
-define('EXTEST_SERIALIZE_ITERATIONS', 10000);
+$iters = isset($argv[1]) ? $argv[1] : 100000;
 
 $static = new ExtestSerializeS;
 $dynamic = new ExtestSerializeD;
 $custom = new ExtestSerializeC;
 
 function test_serialization($object) {
+	global $iters;
+	
 	// output serialization
 	echo serialize($object) . PHP_EOL;
 	$start = microtime(true);
-	for ($j = 0; $j < EXTEST_SERIALIZE_ITERATIONS; $j++) {
+	for ($j = 0; $j < $iters; $j++) {
 		serialize($object);
 	}
 	$end = microtime(true);
 	$duration = $end - $start;
-	printf("time for %d iterations: %f" . PHP_EOL, EXTEST_SERIALIZE_ITERATIONS, $duration);
+	printf("time for %d iterations: %f" . PHP_EOL, $iters, $duration);
 }
 
 for ($i = 0; $i < EXTEST_NUM_EXAMS; $i++) {
