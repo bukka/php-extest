891868b9ff3682c75212be8d1a9dfc5c7de22c58
Jakub Zelenka <bukka@php.net>
2014-11-19 19:49:17 +0000

    Fix string existence test for 7

--- examples/compat.php
+++ examples/compat.php
@@ -1,5 +1,5 @@
 <?php
 //var_dump(extest_compat_long(PHP_INT_MAX, true));
 //var_dump(extest_compat_str("test data"));
-extest_compat_array(array("k1" => "test data", "k2" => 1, "k3" => 2.23));
+//extest_compat_array(array("k1" => "test data", "k2" => 1, "k3" => 2.23));
 extest_compat_array(array("test data", "k2", 2.23));
